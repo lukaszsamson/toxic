0a047c71e098c5789c252bd755979c7788e828e7
Lukasz Samson <lukaszsamson@gmail.com>
2026-01-08 13:30:48 +0100

    wip

--- lib/toxic/driver/deferrals.ex
+++ lib/toxic/driver/deferrals.ex
@@ -1,13 +1,23 @@
 defmodule Toxic.Driver.Deferrals do
   @moduledoc false
 
+  # A3 optimization: Specialize flush/2 and append/3 to avoid unnecessary
+  # list operations when one of the inputs is empty.
+  # Use :lists.reverse/1,2 instead of Enum.reverse/1,2 in hot paths.
+
   @spec flush(list(), list()) :: list()
-  def flush(output, deferrals) do
-    output ++ Enum.reverse(deferrals)
-  end
+  # When output is empty, just reverse deferrals - no concatenation needed
+  def flush([], deferrals), do: :lists.reverse(deferrals)
+  # When deferrals is empty, just return output - no reverse needed
+  def flush(output, []), do: output
+  # General case: concatenate output with reversed deferrals
+  def flush(output, deferrals), do: output ++ :lists.reverse(deferrals)
 
   @spec append(list(), list(), list()) :: list()
-  def append(output, deferrals, tokens) do
-    output ++ Enum.reverse(deferrals, tokens)
-  end
+  # When output is empty, reverse deferrals onto tokens directly
+  def append([], deferrals, tokens), do: :lists.reverse(deferrals, tokens)
+  # When deferrals is empty, just concatenate output with tokens
+  def append(output, [], tokens), do: output ++ tokens
+  # General case: concatenate output with reversed deferrals and tokens
+  def append(output, deferrals, tokens), do: output ++ :lists.reverse(deferrals, tokens)
 end
